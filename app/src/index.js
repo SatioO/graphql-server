0c738eea892c1831d2f06f2b2a689abc629dc667
Brain <vaibhav.satam29991@gmail.com>
2018-02-03 16:39:10 +0530

    restruct(schema): restructured schema

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,13 @@
+import { GraphQLSchema } from "graphql";
+import { BlogQueryRootType } from "./schema";
+import { BlogMutationRootType } from "./mutations";
+
+// This is the schema declaration
+export const BlogAppSchema = new GraphQLSchema({
+	query: BlogQueryRootType,
+	mutation: BlogMutationRootType
+});
+
 export * from "./schema";
 export * from "./resolvers";
 export * from "./mutations";
--- app/src/schema/index.js
+++ app/src/schema/index.js
@@ -1,13 +1,25 @@
-import { GraphQLSchema } from "graphql";
-import { BlogQueryRootType } from "./blog.schema";
-import { BlogMutationRootType } from "../mutations";
+import { GraphQLList, GraphQLObjectType } from "graphql";
+import { AuthorType } from "./author.schema";
+import { PostType } from "./post.schema";
+import { getAuthors, getPosts } from "../resolvers";
 
-// This is the schema declaration
-export const BlogAppSchema = new GraphQLSchema({
-	query: BlogQueryRootType,
-	mutation: BlogMutationRootType
+// This is the Root Query
+export const BlogQueryRootType = new GraphQLObjectType({
+	name: "BlogAppSchema",
+	description: "Blog Application Schema Root",
+	fields: () => ({
+		authors: {
+			type: new GraphQLList(AuthorType),
+			description: "List of all Authors",
+			resolve: getAuthors
+		},
+		posts: {
+			type: new GraphQLList(PostType),
+			description: "List of all Posts",
+			resolve: getPosts
+		}
+	})
 });
 
 export * from "./author.schema";
 export * from "./post.schema";
-export * from "./blog.schema";
