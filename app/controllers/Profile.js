bff42cc75f7f63934e1c627c4def4af09b809259
Brain <vaibhav.satam29991@gmail.com>
2018-02-13 20:30:25 +0530

    fix(search): fixed search structure

--- app/controllers/Profile.js
+++ app/controllers/Profile.js
@@ -99,37 +99,62 @@ export const Profile = {
 			return error;
 		}
 	},
-	findOne: async (_, { id }) => {
+	findOne: async (_, { id, pageOffset = 0, pageLength = 1 }) => {
 		try {
-			const profile = await ProfileModel.findOne({ id: id });
+			const profiles = await client.search({
+				index: "aic",
+				type: "profiles",
+				body: {
+					from: pageOffset,
+					size: pageLength,
+					query: {
+						bool: {
+							must: {
+								term: { profileStatus: "active" }
+							},
+							filter: {
+								term: { id: id }
+							}
+						}
+					}
+				}
+			});
+
 			const locations = Store.getKeys(_, { key: "location" });
 			const designations = Store.getKeys(_, { key: "designation" });
 
-			if (!!profile && !!profile.skills) {
-				for (const skill in profile.skills) {
-					if (typeof profile.skills[skill] === "string") {
-						profile.skills[skill] = [profile.skills[skill]];
+			const result = profiles.hits.hits.map(profile => {
+				if (!!profile && !!profile._source.location) {
+					locations.result.map(location => {
+						if (String(location.id) === profile._source.location) {
+							profile._source.location = location.description;
+						}
+					});
+				}
+				if (!!profile && !!profile._source.designation) {
+					designations.result.map(designation => {
+						if (String(designation.id) === profile._source.designation) {
+							profile._source.designation = designation.description;
+						}
+					});
+				}
+
+				if (!!profile && !!profile._source.skills) {
+					for (const skill in profile._source.skills) {
+						if (typeof profile._source.skills[skill] === "string") {
+							profile._source.skills[skill] = [profile._source.skills[skill]];
+						}
 					}
 				}
-			}
+				return profile._source;
+			});
 
-			if (!!profile && !!profile.location) {
-				locations.result.map(location => {
-					if (String(location.id) === profile.location) {
-						profile.location = location.description;
-					}
-				});
-			}
-
-			if (!!profile && !!profile.designation) {
-				designations.result.map(designation => {
-					if (String(designation.id) === profile.designation) {
-						profile.designation = designation.description;
-					}
-				});
-			}
-
-			return profile;
+			return {
+				pageOffset: pageOffset,
+				pageLength: pageLength,
+				total: profiles.hits.total,
+				items: result
+			};
 		} catch (error) {
 			return error;
 		}
